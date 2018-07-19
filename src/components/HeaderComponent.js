27d5bd40fd651499c332ef04b7bb71eaf9eeff0a
Niculae Niculae <niculaenick@gmail.com>
2018-07-19 14:58:54 +0200

    Add router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,30 +1,92 @@
 import React, {Component} from 'react';
-import {Navbar, NavbarBrand, Jumbotron, Container} from 'reactstrap';
+import {
+    Nav,
+    NavbarToggler,
+    Collapse,
+    NavItem,
+    Navbar,
+    NavbarBrand,
+    Jumbotron,
+    Container
+} from 'reactstrap';
+import {NavLink} from 'react-router-dom';
 
 class Header extends Component {
-    
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isNavOpen: false
+        };
+
+    }
+
+    toggleNav = () => {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+
+    }
     render() {
         return (
             <React.Fragment>
-                <Navbar dark>
+                <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav}/>
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img
+                                src="assets/images/logo.png"
+                                height="30"
+                                width="41"
+                                alt="Ristorante Con Fusion"/>
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg"></span>
+                                        Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-info fa-lg"></span>
+                                        About Us
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg"></span>
+                                        Menu
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg"></span>
+                                        About Us
+                                    </NavLink>
+                                </NavItem>
+
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron fluid>
-                     <Container fluid>
+                    <Container fluid>
                         <div className="row row-header">
                             <div className="col-sm-6">
                                 <h1>Ristorante Con Fusion</h1>
-                                <p>We take inspiration from the world's best cuisines amd create a unique fusion experience. Our lipsmacking creation will tickle you culinary senses.</p>
+                                <p>We take inspiration from the world's best cuisines amd create a unique fusion
+                                    experience. Our lipsmacking creation will tickle you culinary senses.</p>
                             </div>
                         </div>
-                        </Container>  
+                    </Container>
                 </Jumbotron>
-                </React.Fragment>
-            
-            );
+            </React.Fragment>
+
+        );
     }
 }
- 
+
 export default Header;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,35 +4,34 @@ import DishDetail from './DishdetailComponent';
 import {DISHES} from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent'
+import Home from './HomeComponent';
+import {Switch, Route, Redirect} from 'react-router-dom';
 class Main extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      dishes: DISHES,
-      selectedDish: null
+      dishes: DISHES
     };
   }
-  onDishSelect = (id) => {
-    this.setState({selectedDish: id});
-  }
+
   render() {
+
+    const HomePage = ()=>{
+      return ( <Home />);
+    };
+
     return (
-      
+
       <React.Fragment>
         <Header/>
-        <Menu
-          dishes={this.state.dishes}
-          onDishSelect={(id) => {
-          this.onDishSelect(id)
-        }}/>
-        <DishDetail
-          selectedDish={this
-          .state
-          .dishes
-          .filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+          <Switch>
+            <Route path='/home' component={HomePage}  />
+            <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+            <Redirect to='/home' />
+          </Switch>
         <Footer/>
-        </React.Fragment>
+      </React.Fragment>
 
     );
   }
